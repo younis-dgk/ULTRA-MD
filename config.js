91a122fd516d9d6a9174b3e734aa650e703839d5
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-11-30 10:47:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '923194999455';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
